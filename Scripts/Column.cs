831cdbe7e50ba32d4110473199011badf5c03cb0
Виталий <39260909+Pandorrumm@users.noreply.github.com>
2019-09-18 18:29:50 +0700

    Add files via upload

---
+++ Scripts/Column.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Column : MonoBehaviour
+{
+    public GameObject plusScore;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.GetComponent<Hero>() != null)
+        {
+            GameControl.Instance.Score();
+            SoundManager.PlaySound("Score");
+            Instantiate(plusScore,transform.position, Quaternion.identity);
+            
+        }
+    }
+
+}
