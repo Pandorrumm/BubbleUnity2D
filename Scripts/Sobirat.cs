7e310b89b8a9c7f7c2a1f992bc6de52e25e97e7f
Виталий <39260909+Pandorrumm@users.noreply.github.com>
2019-09-18 18:31:28 +0700

    Add files via upload

---
+++ Scripts/Sobirat.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sobirat : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+    public GameObject plusScore;
+
+    private Vector2 objectInsertPositionPuzir;
+    private PuzirInsert puzirInsert;
+
+    public float columnYMin = -2f;
+    public float columnYMax = 2f;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        puzirInsert = FindObjectOfType<PuzirInsert>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "hero")
+        {
+            GameControl.Instance.Score();
+            SoundManager.PlaySound("Score");
+            Destroy(gameObject);
+            float spawnXPos = 13f;
+            float spawnYPos = Random.Range(columnYMin, columnYMax);
+            objectInsertPositionPuzir = new Vector2(spawnXPos, spawnYPos);
+
+            Instantiate(puzirInsert.puzirPrefab, objectInsertPositionPuzir, Quaternion.identity);
+
+            Instantiate(plusScore, transform.position, Quaternion.identity);
+
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "deadPuzir")
+        {
+            Destroy(gameObject);
+            Instantiate(puzirInsert.puzirPrefab, objectInsertPositionPuzir, Quaternion.identity);
+        }
+    }
+}
