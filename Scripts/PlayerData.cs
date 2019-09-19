aa8687e1c67fc68da437c25b3c7f69d597ab7b42
Виталий <39260909+Pandorrumm@users.noreply.github.com>
2019-09-19 17:11:55 +0700

    Add files via upload

---
+++ Scripts/PlayerData.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class PlayerData 
+{
+    public int deadCounter;
+    
+    public PlayerData(GameControl gameControl)
+    {
+        deadCounter = GameControl.deadCounter;
+        
+    }
+
+}
---
+++ Scripts/SaveScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+public static class SaveScript 
+{
+    public static void SavePlayer(GameControl gameControl)
+    {
+        BinaryFormatter formatter = new BinaryFormatter();
+
+        string path = Application.persistentDataPath + "/gamesave.fun";
+        FileStream stream = new FileStream(path, FileMode.Create);
+
+        PlayerData data = new PlayerData(gameControl);
+
+        formatter.Serialize(stream, data);
+
+        stream.Close();
+
+    }
+
+    public static PlayerData LoadPlayer()
+    {
+        string path = Application.persistentDataPath + "/gamesave.fun";
+
+        if (File.Exists(path))
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream stream = new FileStream(path, FileMode.Open);
+
+            PlayerData data = formatter.Deserialize(stream) as PlayerData;
+            stream.Close();
+
+            return data;
+        }
+        else
+        {
+            Debug.LogError("Нет сохранений " + path);
+            return null;
+        }
+    }
+}
