1ad70ca70c7ce1e08e5ad6a98415972a8a0e68e0
Виталий <39260909+Pandorrumm@users.noreply.github.com>
2019-09-23 10:35:23 +0700

    Add files via upload

---
+++ Scripts/GameControl.cs
@@ -0,0 +1,142 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class GameControl : MonoBehaviour
+{
+    public static GameControl Instance; //экземпляр
+
+    public float scrollSpeed = -1.5f;
+    public bool isGameOver = false;
+    private int score = 0;
+    public int MaxScore;
+    public Text scoreText;
+    public Text MaxScoreText;
+    
+    public GameObject gameOverText;
+    public GameObject gameOverPanel;
+    public GameObject readyPanel;
+
+    public AdMobInterstitial ad;
+    public static int deadCounter = 0; //счётчик смертей
+
+    private static PlayerData playerData;
+
+    private ParallaxBackGround parallaxBackGround;
+
+    private PauseButton pause;
+
+    void Start()
+    {
+        pause = FindObjectOfType<PauseButton>();
+        parallaxBackGround = FindObjectOfType<ParallaxBackGround>();
+
+        MaxScore = PlayerPrefs.GetInt("MaxScore");
+        readyPanel.SetActive(true);
+        pause.pauzePanel.SetActive(true);
+        Time.timeScale = 0;
+        
+    }
+
+    void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+        }else if(Instance != null)
+        {
+            Destroy(gameObject);
+        }      
+    }
+
+    void Update()
+    {
+        
+        if(MaxScore < score)
+        {
+            MaxScore = score;
+            PlayerPrefs.SetInt("MaxScore", MaxScore);
+        }
+        if(MaxScore > score)
+        {
+            MaxScoreText.text = "Рекорд: " + MaxScore;
+        }
+    }
+
+    public void Score()
+    {
+        if(isGameOver)
+        {
+            return;
+        }
+        score++;
+        scoreText.text = "Очки: " + score;
+    }
+
+    public void Die()
+    {
+        isGameOver = true;      
+        Invoke("GameOverTextActiv", 1f);
+    }
+
+    void GameOverTextActiv()
+    {
+        //gameOverText.SetActive(true);
+        gameOverPanel.SetActive(true);
+    }
+
+    public void Restart()
+    {
+        
+        //PlayerPrefs.SetInt("deadCounter", deadCounter);
+        deadCounter++;
+        Debug.Log(deadCounter);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);   
+    }
+
+    public void ReadyPanel()
+    {
+        pause.pauzePanel.SetActive(false);
+        readyPanel.SetActive(false);
+        Time.timeScale = 1;
+        SoundManager.PlaySound("CreateBooble");
+        
+    }
+
+
+    public void Exit()
+    {
+        SceneManager.LoadScene("MenuGame");
+        
+    }
+
+    public void AdmobShow()
+    {
+        if (deadCounter % 3 == 0)
+        {
+            ad.ShowAds();
+            Debug.Log(deadCounter);
+            Debug.Log("показ баннера");
+        }
+    }
+
+    public void SavePlayer()
+    {
+        SaveScript.SavePlayer(this);
+        Debug.Log("Сохранили");
+
+    }
+
+    public void Load()
+    {
+        
+        // SceneManager.LoadScene("ChoiceLevels");
+       // PlayerData data = SaveScript.LoadPlayer();
+       // GameControl.deadCounter = data.deadCounter;
+        Debug.Log("Загрузили");
+    }
+
+}
---
+++ Scripts/PauseButton.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseButton : MonoBehaviour
+{
+   
+    bool isPaused = false;
+    public GameObject pauzePanel;
+
+    public void PauseGame()
+    {
+        if (isPaused)
+        {
+            pauzePanel.SetActive(false);
+            Time.timeScale = 1;
+            isPaused = false;
+        }
+        else
+        {
+            pauzePanel.SetActive(true);
+            Time.timeScale = 0;
+            isPaused = true;
+        }
+    }
+}
