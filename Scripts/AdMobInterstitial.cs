3e59f3eed9ab9ef1cfd7cbc59f36c3bcb0ff189b
Виталий <39260909+Pandorrumm@users.noreply.github.com>
2020-04-14 10:09:48 +0700

    Add files via upload

---
+++ Scripts/AdMobInterstitial.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleMobileAds.Api;
+using UnityEngine.SceneManagement;
+using System;
+
+public class AdMobInterstitial : MonoBehaviour
+{
+    public string AppID;
+    public string InterstitialAdUnitID;
+    private InterstitialAd interstitialAd;
+    
+
+    void Start()
+    {
+        MobileAds.Initialize(AppID);
+        interstitialAd = new InterstitialAd(InterstitialAdUnitID);
+        interstitialAd.LoadAd(new AdRequest.Builder().Build());
+        interstitialAd.OnAdClosed += HandlerOnClosed;
+    }
+
+    public void HandlerOnClosed(object sendler, EventArgs args)
+    {       
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void ShowAds()
+    {
+        if (interstitialAd.IsLoaded())
+        {
+            interstitialAd.Show();
+        }
+        else
+        {           
+            SceneManager.LoadScene("SampleScene");
+        }
+        
+    }
+}
